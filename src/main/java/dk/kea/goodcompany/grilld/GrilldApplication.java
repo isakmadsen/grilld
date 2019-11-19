e8dd06350e28795380c791d627bdc2f959fb3a02
Issa <issa0047@stud.kea.dk>
2019-11-19 13:17:27 +0100

    Initial commit

---
+++ src/main/java/dk/kea/goodcompany/grilld/GrilldApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.goodcompany.grilld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrilldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrilldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/goodcompany/grilld/GrilldApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.goodcompany.grilld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrilldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
