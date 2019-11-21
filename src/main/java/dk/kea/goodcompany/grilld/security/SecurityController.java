50fb1b910f5471feffdb1c6a13dd2e97aa848f66
paulkapa <paulkapa@yahoo.com>
2019-11-21 17:44:10 +0100

    I. Functionality for Manager login with extension for Staff and Customer login. TO-DO next: - Improve display method -did - Update welcome page so that the dropdown works -solved - Make sure Security is solid -check II. Created "templates/platform_users" directory TO-DO next: - Update Account Pages with Proper Information -in progress

--- src/main/java/dk/kea/goodcompany/grilld/security/SecurityController.java
+++ src/main/java/dk/kea/goodcompany/grilld/security/SecurityController.java
@@ -2,22 +2,24 @@ package dk.kea.goodcompany.grilld.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SecurityController {
 
+    // Manager = 1; Staff = 2; Customer = 3
     int logInAccess;
 
     public SecurityController() {
-
+        logInAccess = 0;
     }
 
-    @GetMapping("/login")
-    public String getLoginPage() {
+    @GetMapping("/login/{accountType}")
+    public String getLoginPage(@PathVariable("accountType") int type) {
 
-        logInAccess = 0;
+        logInAccess = type;
 
         return "security/login";
     }
@@ -28,16 +30,25 @@ public class SecurityController {
         username = username.trim().toLowerCase();
         password = password.trim().toLowerCase();
 
-        if (username.equals("customer") && password.equals("1234")) {
-            logInAccess = 1;
-            return (username + "/account").toString();
-        } else if (username.equals("manager") && password.equals("1234")) {
-            logInAccess = 2;
-            return (username + "/account").toString();
-        } else {
-            return "redirect:/login";
+        //TODO - SecurityController - Implement a way to check the database for account details
+
+        switch (logInAccess) {
+            case 1:
+                if (username.equals("manager") && password.equals("1234"))
+                    return ("platform_users/" + username + "/account");
+                else return ("redirect:/login/" + logInAccess);
+            case 2:
+                if (username.equals("staff") && password.equals("1234"))
+                    return ("platform_users/" + username + "/account");
+                else return ("redirect:/login/" + logInAccess);
+            case 3:
+                if (username.equals("customer") && password.equals("1234"))
+                    return ("platform_users/" + username + "/account");
+                else return ("redirect:/login/" + logInAccess);
+            default: {
+                logInAccess = 0;
+                return "redirect:/";
+            }
         }
     }
-
-
 }
